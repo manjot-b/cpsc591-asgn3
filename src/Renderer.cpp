6ff67e472068b71ab7e853c9403552cb426eb7c9
Manjot <mbal29@hotmail.com>
2020-11-08 11:40:41 -0700

    Change background color.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -140,7 +140,7 @@ void Renderer::run()
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.9f, 0.85f, 0.88f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		processWindowInput();
